c06330d7276bf7e7f53fe119e42d220581698625
Mateusz Fabisiak <mateusz.fabisiak.87@gmail.com>
2021-08-16 17:17:41 +0100

    ADD titleClickHandler code

--- js/script.js
+++ js/script.js
@@ -8,9 +8,10 @@ document.getElementById('test-button').addEventListener('click', function(){
 */
 
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked!');
-    console.log(event);
+//    console.log('Link was clicked!');
+//    console.log(event);
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -19,7 +20,7 @@ const titleClickHandler = function(event){
     }
     /* add class 'active' to the clicked link */
     clickedElement.classList.add('active');
-    console.log('clickedElement', clickedElement);
+//    console.log('clickedElement', clickedElement);
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
@@ -27,10 +28,13 @@ const titleClickHandler = function(event){
         activeArticle.classList.remove('active');
     }
     /* get 'href' attribute from the clicked link */
-  
+    const articleSelector = clickedElement.getAttribute('href');
+//    console.log(articleSelector);
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    const targetArticle = document.querySelector(articleSelector);
+ //   console.log(targetArticle);
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
