c3e54288997335a79fa36d2b42af8a8048602aa8
Mateusz Fabisiak <mateusz.fabisiak.87@gmail.com>
2021-08-23 19:03:55 +0100

    UPDATE to fully working script

--- js/script.js
+++ js/script.js
@@ -2,11 +2,41 @@
   'use strict';
 
   /*
-    document.getElementById('test-button').addEventListener('click', function(){
-        const links = document.querySelectorAll('.titles a');
-        console.log('links:', links);
-    });
-    */
+  document.getElementById('test-button').addEventListener('click', function(){
+    const links = document.querySelectorAll('.titles a');
+    console.log('links:', links);
+  };
+  */
+
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+  const generateTitleLinks = function() {
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+    /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = '';
+
+    for(let article of articles){
+      /* get the article id */
+      const articleID = article.getAttribute('id');
+      /* find the title element */
+      /* get the title from the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      /* create HTML of the link */
+      const linkHTML = '<li><a href="#' + articleID + '"><span>' + articleTitle + '</span></a></li>';
+      /* insert link into titleList */
+      html = html + linkHTML;
+    }
+    
+    titleList.innerHTML = html;
+  };
+
+  generateTitleLinks();
 
   const titleClickHandler = function(event){
     event.preventDefault(); // zapobiega zmianie adresu - strona zostaje na tej samej wysokości
@@ -30,42 +60,10 @@
     /* add class 'active' to the correct article */
     targetArticle.classList.add('active');
   };
+
+  const links = document.querySelectorAll('.titles a');
     
-  const optArticleSelector = '.post',
-    optTitleSelector = '.post-title',
-    optTitleListSelector = '.titles';
-
-  const generateTitleLinks = function() {
-    /* remove contents of titleList */
-    const titleList = document.querySelector(optTitleListSelector);
-    titleList.innerHTML = '';
-    /* for each article */
-    const articles = document.querySelectorAll(optArticleSelector);
-    for(let article of articles){
-      article.innerHTML = '';
-    }
-
-    let html = '';
-
-    for(let article of articles){
-      /* get the article id */
-      const articleID = article.getAttribute('id');
-      /* find the title element */
-      /* get the title from the title element */
-      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-      /* create HTML of the link */
-      const linkHTML = '<li><a href="#' + articleID + '"><span>' + articleTitle + '</span></a></li>';
-      /* insert link into titleList */
-      html = html + linkHTML;
-    }
-
-    const links = document.querySelectorAll('.titles a');
-    
-    for(let link of links){
-      link.addEventListener('click', titleClickHandler);
-    }
-
-    titleList.innerHTML = html;
-  };
-  generateTitleLinks();
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
 }
